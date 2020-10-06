1018ae3bf3570837ad0dcaa5bc461b3cfa675221
my name <you@exampl.com>
2020-10-06 10:26:23 +0600

    backend workings all done well

--- index.js
+++ index.js
@@ -2,12 +2,39 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const mongodb = require('mongodb')
+const MongoClient = require('mongodb').MongoClient;
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g0cgl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+
+client.connect(err => {
+    const menuItems = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_TABLE_ONE}`);
+    const whyChooseUs = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_TABLE_TWO}`);
+    const foodOrder = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_TABLE_THREE}`);
+    console.log("Database is connected");
+
+    app.get('/', (req, res) => res.send("<h1>Read Onion Foods</h1>"))
+
+    app.get('/menuItems', (req, res) => {
+        menuItems.find({})
+            .toArray((err, result) => res.send(result))
+    })
+
+    app.get('/whyChooseUs', (req, res) => {
+        whyChooseUs.find({}).toArray((err, result) => res.send(result))
+    })
+
+    app.post('/foodOrderConfirm', (req, res) => {
+        foodOrder.insertOne(req.body)
+            .then(result => res.send(result))
+    })
+});
+
 
 app.listen(4000, () => {
     console.log("Server is running")
